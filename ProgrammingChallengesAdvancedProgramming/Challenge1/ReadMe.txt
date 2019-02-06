e9dcbb55f13895dc16f769b057aee9a87fd6dff2
Ali Villegas <alibryan.villegas@gmail.com>
2019-02-05 21:44:39 -0600

    Challenge 1
    Version 1.0

---
+++ ProgrammingChallengesAdvancedProgramming/Challenge1/ReadMe.txt
@@ -0,0 +1,6 @@
+Implementation of linked list using structures.
+Language: C
+
+Julián Herrera A01114097
+Santiago Bárcenas A01652520
+Ali Villegas A01337596
---
+++ ProgrammingChallengesAdvancedProgramming/Challenge1/main.c
@@ -0,0 +1,266 @@
+#include <stdio.h>
+
+struct Node {
+   int  age;
+   char *name;
+   struct Node * nextNode;
+};
+
+struct LinkedList{
+   int size;
+   struct Node * firstNode;
+};
+
+//adds item to the end of the list
+int addItem(struct LinkedList *l, struct Node *item){
+   if(l->size == 0){
+      l->firstNode = item;
+      struct Node *indexNode  = l->firstNode;
+      indexNode->nextNode = NULL;
+      l->size = l->size +1;
+   }
+   else if(l->size == 1){
+     struct Node *indexNode  = l->firstNode;
+     indexNode->nextNode = item;
+     struct Node *indexNode2  = indexNode->nextNode;
+     indexNode2->nextNode = NULL;
+     l->size = l->size +1;
+   }
+   else{
+     struct Node *indexNode  = l->firstNode;
+     while(indexNode->nextNode != NULL){
+          indexNode = indexNode->nextNode;
+      }
+      indexNode->nextNode = item;
+      struct Node *indexNode2  = indexNode->nextNode;
+      indexNode2->nextNode = NULL;
+      l->size = l->size +1;
+   }
+   return 0;
+}
+
+//pushes an item in the first position of the list
+int pushItem(struct LinkedList *l, struct Node *item){
+   if(l->size == 0){
+      l->firstNode = item;
+      struct Node *indexNode  = l->firstNode;
+      indexNode->nextNode = NULL;
+      l->size = l->size +1;
+   }
+   else{
+     struct Node *indexNode  = l->firstNode;
+     l->firstNode = item;
+     struct Node *indexNode2  = l->firstNode;
+     indexNode2->nextNode = indexNode;
+     l->size = l->size +1;
+   }
+   return 0;
+}
+
+//Prints the list
+int printLinkedList(struct LinkedList *l){
+  if(l->size== 0){
+    printf("The list has no elements");
+  }
+  else if(l->size==1){
+    int index = 0 ;
+    struct Node indexNode  = *l->firstNode;
+         printf("\n%d--Age: %d Name: %s", index, indexNode.age,indexNode.name);
+    }
+   else{
+      int index = 0 ;
+      struct Node * indexNode  = l->firstNode;
+      while(indexNode->nextNode != NULL){
+          printf("\n%d--Age: %d Name: %s", index, indexNode->age,indexNode->name);
+          index++;
+          indexNode = indexNode->nextNode;
+      }
+      printf("\n%d--Age: %d Name: %s", index, indexNode->age,indexNode->name);
+   }
+
+  return 0;
+}
+
+//Removes first item on the list
+int popItem(struct LinkedList *l){
+  if(l->size == 0){
+     printf("There's nothing to pop");
+  }
+  else{
+      struct Node *indexNode  = l->firstNode;
+      l->firstNode = NULL;
+      l->firstNode =  indexNode->nextNode;
+      l->size = l->size - 1;
+  }
+
+  return 0;
+}
+
+//Removes last item of the list
+int removeLastItem(struct LinkedList *l){
+   if(l->size== 0){
+    printf("\nThe list already has no elements\n");
+  }
+  else if(l->size==1){
+    l->firstNode = NULL;
+    l->size = l->size - 1;
+    }
+   else{
+      struct Node * indexNode  = l->firstNode;
+      struct Node * previousNode = NULL;
+      while(indexNode->nextNode != NULL){
+          previousNode = indexNode;
+          indexNode = indexNode->nextNode;
+      }
+      previousNode->nextNode = NULL;
+      indexNode = NULL;
+      l->size = l->size - 1;
+   }
+
+  return 0;
+}
+
+//Removes the item on the given index
+int removeItem(struct LinkedList *l, int index){
+  if(l->size== 0){
+    printf("The list has no elements \n");
+  }
+  else if(l->size -1 < index){
+    printf("The list doesn't have that index \n");
+  }
+  else if(index == 0 && l->size == 1){
+    l->firstNode = NULL;
+    l->size = l->size - 1;
+  }
+  else if(index == 0){
+    struct Node * indexNode  = l->firstNode;
+    l->firstNode = indexNode->nextNode;
+    indexNode = NULL;
+    l->size = l->size - 1;
+  }
+  else{
+    if(index == l->size-1){
+      struct Node * indexNode  = l->firstNode;
+      struct Node * previousNode  = NULL;
+      while(indexNode->nextNode != NULL){
+          previousNode = indexNode;
+          indexNode = indexNode->nextNode;
+          }
+       previousNode->nextNode = NULL;
+       indexNode = NULL;
+      }
+    else{
+      int i = 0;
+      struct Node * indexNode  = l->firstNode;
+      struct Node * previousNode  = NULL;
+      while(indexNode->nextNode != NULL){
+          previousNode = indexNode;
+          indexNode = indexNode->nextNode;
+          i++;
+          if(i == index){
+            break;
+          }
+        }
+       previousNode->nextNode = indexNode->nextNode;
+       indexNode = NULL;
+      }
+    }
+  return 0;
+}
+
+//Returns the size of the list
+int getSize(struct LinkedList *l){
+  printf("The size of the list is: %d\n", l->size);
+  return 0;
+}
+
+//Retrieves the node on a given index
+int getItem(struct LinkedList *l){
+  int n;
+  printf("\nType the index you want to retrieve: ");
+  scanf("%d", &n);
+  if(l->size== 0){
+    printf("The list has no elements \n");
+  }
+  if(l->size -1 < n){
+    printf("The list doesn't have that index \n");
+  }
+  else if(l->size==1 && n == 0){
+    int index = 0;
+    struct Node indexNode  = *l->firstNode;
+         printf("\n%d--Age: %d Name: %s \n", index, indexNode.age,indexNode.name);
+    }
+    else {
+      int index = 0 ;
+      struct Node * indexNode  = l->firstNode;
+      if(n == 0) {
+        printf("\n%d--Age: %d Name: %s \n", index, indexNode->age,indexNode->name);
+        return 0;
+      }
+      while(indexNode->nextNode != NULL){
+          index++;
+          indexNode = indexNode->nextNode;
+          if(index == n) {
+            printf("\n%d--Age: %d Name: %s \n", index, indexNode->age,indexNode->name);
+            return 0;
+          }
+      }
+    }
+  return 0;
+}
+
+//Deletes every node on the list
+int clearList(struct LinkedList *l){
+  if(l->size == 0)
+    printf("List has no elements to clear");
+  else if(l->size == 1){
+    l->firstNode = NULL;
+  }
+  else{
+    struct Node *indexNode = l->firstNode;
+    struct Node * previousNode = NULL;
+    while(indexNode->nextNode != NULL){
+      previousNode = indexNode;
+      indexNode = indexNode->nextNode;
+      previousNode->nextNode = NULL;
+      previousNode = NULL;
+    }
+    l->firstNode = NULL;
+    l->size = 0;
+  }
+  return 0;
+}
+
+
+
+int main(void) {
+  //linked list implementation using structures
+  struct Node item1;
+  struct Node item2;
+  struct Node item3;
+  struct Node item4;
+  struct Node item5;
+
+  item1.age = 15;
+  item1.name= "Malcolm";
+  item2.age = 20;
+  item2.name = "Felicity";
+  item3.age = 13;
+  item3.name = "Julius";
+  item4.age = 69;
+  item4.name = "Clark";
+  item5.age = 18;
+  item5.name = "Travolta";
+  struct LinkedList list;
+  list.size = 0;
+
+  addItem(&list, &item2);
+  addItem(&list, &item3);
+  pushItem(&list, &item4);
+  addItem(&list, &item1);
+  getSize(&list);
+  printf("Printing List after filling:\n");
+  printLinkedList(&list);
+
+  return 0;
+}
